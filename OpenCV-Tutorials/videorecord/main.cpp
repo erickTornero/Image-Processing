2392b8958d9029c12073729e16dc5769be76644d
wzuniga <wzuniga@episunsa.edu.pe>
2018-12-03 22:29:21 -0500

    Adding new parameters

--- OpenCV-Tutorials/videorecord/main.cpp
+++ OpenCV-Tutorials/videorecord/main.cpp
@@ -10,33 +10,56 @@ int main(){
   // Create a VideoCapture object and open the input file
   // If the input is the web camera, pass 0 instead of the video file name
   //VideoCapture cap(0);
-  VideoCapture cap("/home/erick/Documentos/MsCS/Images/VideosPrueba/VideosPrueba/PadronCirculos_03.avi");
+  VideoCapture cap("../../../files/PadronAnillos_02.avi");
   // Check if camera opened successfully
   if(!cap.isOpened()){
     std::cout << "Error opening video stream or file" << std::endl;
     return -1;
   }
-     
+  
+  cv::SimpleBlobDetector::Params params;
+  params.minThreshold = 10;
+  params.maxThreshold = 200;
+  // Filter by Area.
+	params.filterByArea = true;
+	params.minArea = 100;
+  // Filter by Circularity
+  params.filterByCircularity = true;
+  params.minCircularity = 0.8;
+  // Filter by Convexity
+  params.filterByConvexity = true;
+  params.minConvexity = 0.9;
+  // Filter by Inertia
+  params.filterByInertia = true;
+  params.minInertiaRatio = 0.01;
+
   while(1){
  
     Mat frame;
-    Mat dest_gray, border_gray;
+    Mat dest_gray, border_gray, laplacian, src;
     
     // Capture frame-by-frame
     cap >> frame;
+    //GaussianBlur( frame, src, Size(3, 3), 0, 0, BORDER_DEFAULT );
     cv::cvtColor(frame, dest_gray, cv::COLOR_BGR2GRAY);
+    //imshow( "Black", dest_gray );
+    //cv::Canny( dest_gray, border_gray, 200, 300, 3 );
+    
     cv::Canny( dest_gray, border_gray, 200, 300, 3 );
+    cv::Laplacian( border_gray, laplacian, CV_8U );
+    imshow( "Canny", laplacian );
     std::vector<cv::KeyPoint> keypoints;
-    cv::SimpleBlobDetector::Params params;
-    params.minThreshold = 10;
-    params.maxThreshold = 200;
+    
     cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
-    detector->detect(border_gray, keypoints);
+    detector->detect(laplacian, keypoints);
     // If the frame is empty, break immediately
     Mat drawI = border_gray.clone();
-    for(size_t i = 0; i < keypoints.size(); i++){
-        cv::circle(frame, keypoints[i].pt, 4, cv::Scalar(180, 0, 0), -1);
 
+    //std::cout << "KeyPoint size = " << keypoints.size() << std::endl;
+    for(size_t i = 0; i < keypoints.size(); i++){
+        cv::circle(frame, keypoints[i].pt, 5, cv::Scalar(180,180,0), 3);
+        //std::cout << i << " KeyPoint = " << keypoints[i].pt << std::endl;
+        cv::putText(frame, std::to_string(i), keypoints[i].pt, cv::FONT_HERSHEY_SIMPLEX , 0.5, (255,255,255), 1, cv::LINE_AA);
     }
     if (frame.empty())
       break;
